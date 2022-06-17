f96345da735094f78b105ead2800160989d560ca
imanuelcdw <icrucifixio@gmail.com>
2022-06-17 21:11:23 +0700

    add route user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { register, login } = require('../controllers/user')
+const { register, login, getAll } = require('../controllers/user')
 
 router.route('/login').post(login)
 router.route('/register').post(register)
+router.route('/get').get(getAll)
 
 module.exports = router
